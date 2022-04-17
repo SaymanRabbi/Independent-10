ba17de0c09e235807f4e50b9bf51ec36cc59c281
sayman <mdrabbi.asm@gmail.com>
2022-04-17 11:20:03 +0600

    Explore More

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -9,18 +9,27 @@ const Hero = () => {
             <div className='container'>
                 <h1 className='display-1' style={{fontWeight:"600"}}>Explore</h1>
                 <h1 className='display-1' style={{fontWeight:"600"}}>Amazing Places</h1>
-                <button className='button'>Learn More</button>
+                <button className='button'>Learn More &rarr;</button>
             </div>
                 </div>
-                </div>
+            </div>
+            
+         
+
+
+
+
+
             {/* Explre Section-------- */}
             <div className="explore row g-0">
+                <h1 className='text-center display-2 mb-5' style={{fontWeight:"500"}}>Explore <span className='explore-more'>More</span> </h1>
                 <div className="col-6 d-flex align-items-center justify-content-center">
                     <div className='w-75 mx-auto'>
+                        <p>LET THE JOURNEY BEGIN</p>
                     <h2 className='display-2' style={{fontWeight:'600'}}>Explore Now</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl vitae magna pulvinar laoreet.</p>
-                    <button className='button'>View Guide</button>
+                    <button className='button'>View Guide &rarr;</button>
                     </div>
                 </div>
                 <div className="col-6">
